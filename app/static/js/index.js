3ad7ece0d6b79655777d513f485641852a517bfe
cirodevita <Anomalo97>
2024-10-02 12:26:09 +0200

    Added noimage in popup

--- app/static/js/index.js
+++ app/static/js/index.js
@@ -93,7 +93,7 @@ fetch('/instruments')
             
             let popupContent = `
                 <div class="popup-content">
-                    ${instrument.image ? `<img src="${instrument.image}" class="popup-image" alt="Instrument Image">` : ''}
+                    ${instrument.image ? `<img src="${instrument.image}" class="popup-image" alt="Instrument Image">` : `<img src="static/images/noimage.png" class="popup-image" alt="Instrument Image">`}
                     <div class="popup-details">
                         <b>ID:</b> <a href="https://api.meteo.uniparthenope.it/grafana/d/edf1iu0nyyv40e/dashboard?orgId=1&refresh=30s&from=now-24h&to=now&var-stations=${instrument.id}&kiosk" target="_blank">${instrument.id}</a><br>
                         <b>Organization:</b> ${instrument.organization}<br>
